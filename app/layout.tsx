ba615e27df4e0d62af6eddf2a6e284d8bf7cfe1d
Pierre Sköldborg <pierre@progster.se>
2025-05-13 13:17:38 +0200

    Initial - create next app w/ clean up

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const metadata: Metadata = {
+  title: 'BeSocial',
+  description: 'A social feed for social people',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} font-sans antialiased`}>
+        <main className="py-4 px-8 md:py-8 md:px-12">{children}</main>
+      </body>
+    </html>
+  )
+}
