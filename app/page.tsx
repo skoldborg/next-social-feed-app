364022b776da81583db33f9301a841bad802041e
Pierre Sköldborg <pierre@progster.se>
2025-05-14 17:33:33 +0200

    Form for creating a post

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import { PostForm } from '@/components/PostForm'
 import { PostList } from '@/components/PostList'
 
 export default async function Home() {
   return (
-    <div className="max-w-lg m-auto">
-      <h1 className="text-3xl font-bold mb-8">Feed</h1>
-      <PostList />
-    </div>
+    <>
+      <div className="max-w-lg m-auto">
+        <h1 className="text-3xl font-bold mb-8">Feed</h1>
+        <PostList />
+      </div>
+
+      <div className="fixed bottom-4 right-4 bg-zinc-800 p-2 rounded-lg">
+        <h2 className="font-bold mb-4 ml-2">Be social - Add your own post!</h2>
+        <PostForm />
+      </div>
+    </>
   )
 }
---
+++ components/PostForm.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { ActionResponse } from '@/app/actions'
+import { addPost } from '@/lib/features/posts/postsSlice'
+import { useAppDispatch } from '@/lib/hooks'
+import Form from 'next/form'
+import { useActionState } from 'react'
+import cx from 'classnames'
+
+const initialState: ActionResponse = {
+  success: false,
+  message: '',
+  errors: undefined,
+}
+
+export const PostForm = () => {
+  const dispatch = useAppDispatch()
+
+  // Use useActionState hook for the form submission action
+  const [state, formAction, isPending] = useActionState<
+    ActionResponse,
+    FormData
+  >(async (prevState: ActionResponse, formData: FormData) => {
+    try {
+      await dispatch(addPost(formData))
+
+      // Handle successful submission
+      console.log('success')
+      return {
+        success: true,
+        message: '',
+        error: undefined,
+      }
+    } catch (err) {
+      return {
+        success: false,
+        message: (err as Error).message || 'An error occurred',
+        errors: undefined,
+      }
+    }
+  }, initialState)
+
+  const inputClasses =
+    'flex h-10 w-full  bg-white text-zinc-800 text-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50'
+  const textareaClasses =
+    'flex min-h-[80px] w-full bg-white text-zinc-800 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50'
+
+  return (
+    <Form action={formAction} className="p-2" formEncType="multipart/form-data">
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2">Your name</label>
+        <input
+          type="text"
+          name="author"
+          className={cx(inputClasses, 'px-3 py-2')}
+          disabled={isPending}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2">Message</label>
+        <textarea
+          name="content"
+          className={textareaClasses}
+          rows={4}
+          disabled={isPending}
+          required
+        ></textarea>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-bold mb-2">
+          Upload an avatar image
+        </label>
+        <input
+          type="file"
+          name="avatar"
+          accept="image/*"
+          disabled={isPending}
+          className={cx(
+            inputClasses,
+            'file:h-full file:px-4 file:border-0 file:bg-gray-300 file:text-zinc-800 file:mr-2'
+          )}
+        />
+      </div>
+      <button
+        disabled={isPending}
+        type="submit"
+        className="bg-gray-300 text-zinc-800 px-4 py-2"
+      >
+        Submit
+      </button>
+    </Form>
+  )
+}
